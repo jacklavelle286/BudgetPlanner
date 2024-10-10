ee43a261d792217f264c6d3580b8ae3696e9634b
jacklavelle286 <lavellej286@gmail.com>
2024-10-11 00:22:37 +0100

    finished modal

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ function toggleMenu() {
  navLinks.classList.toggle("show");
  
 }
+// Get the modal
+var modal = document.getElementById("income-modal");
+// Get the button that opens the modal
+var btn = document.getElementById("modal-button")
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+ modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+ if (event.target == modal) {
+   modal.style.display = "none";
+ }
+}
+
+document.addEventListener('input', function (event) {
+  if (event.target.tagName.toLowerCase() === 'textarea') {
+    autoGrow(event.target);
+  }
+});
+
+function autoGrow(textarea) {
+  textarea.style.height = 'auto';  // Reset height
+  textarea.style.height = textarea.scrollHeight + 'px';  // Set new height based on content
+}
