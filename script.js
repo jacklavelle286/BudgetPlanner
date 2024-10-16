426b57095448bd185ac92fa71acd4a8586c34426
jacklavelle286 <lavellej286@gmail.com>
2024-10-16 22:58:50 +0100

    fixing transparent modal and making create budget page

--- script.js
+++ script.js
@@ -3,29 +3,32 @@ function toggleMenu() {
   navLinks.classList.toggle("show");
   
  }
- // Get the modal
- var modal = document.getElementById("income-modal");
- // Get the button that opens the modal
- var btn = document.getElementById("modal-button")
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
- // When the user clicks on the button, open the modal
- btn.onclick = function() {
-  modal.style.display = "block";
- }
- 
- // When the user clicks on <span> (x), close the modal
- span.onclick = function() {
+// Get the modal
+var modal = document.getElementById("income-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Function to open the modal
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+
+// Function to close the modal when the close button (x) is clicked
+span.onclick = function() {
   modal.style.display = "none";
- }
- 
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
+}
+
+// Function to close the modal if the user clicks anywhere outside the modal content
+window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
- }
- 
+}
+
  
  document.addEventListener('input', function (event) {
    if (event.target.tagName.toLowerCase() === 'textarea') {
