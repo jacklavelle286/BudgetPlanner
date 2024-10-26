ca31c69b5d9726b924886c77d9574004ebd8de13
jacklavelle286 <lavellej286@gmail.com>
2024-10-26 22:26:37 +0100

    creating login page and added image

--- login.js
+++ login.js
@@ -1,6 +1,14 @@
-window.onload = function() {
- const images = document.querySelectorAll('img');
- images.forEach(img => {
-     img.classList.add('loaded');
- });
-};
+const h1Element = document.querySelector('.savely-h1');
+const modal = document.querySelector('.savely-p');
+
+// Show modal on hover over h1
+h1Element.addEventListener('click', () => {
+  modal.classList.add('modal-active');
+});
+
+// Hide modal on click outside of it
+document.addEventListener('click', (e) => {
+  if (!modal.contains(e.target) && e.target !== h1Element) {
+    modal.classList.remove('modal-active');
+  }
+});
